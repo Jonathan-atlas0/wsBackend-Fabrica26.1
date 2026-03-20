fef23032eac6ecfa032b505fedbb7556dc63c786
Jonathan <JonathanBatista061@gmail.com>
2026-03-20 20:05:56 -0300

    feat: Adicionei o moldes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Verificacao(models.Model):
+    tipo = models.CharField(max_length=50)  
+    url = models.CharField(max_length=500) 
+    resultado = models.CharField(max_length=50)  
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tipo} - {self.consulta}"
