c57ced380edcdee36371a68d7b29b6ceb991d91b
Jonathan <JonathanBatista061@gmail.com>
2026-03-20 20:55:18 -0300

    Feat: Banco extremamente bugado mas funcionando

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Verificacao(models.Model):
-    tipo = models.CharField(max_length=50)  
-    url = models.CharField(max_length=500) 
-    resultado = models.CharField(max_length=50)  
+class Historico(models.Model):
+    tipo = models.CharField(max_length=50)
+    url = models.CharField(max_length=500)
+    resultado = models.CharField(max_length=50)
     data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.tipo} - {self.consulta}"
+        return f"{self.tipo} - {self.url}"
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .virustotal import VirusTotalService
+from .models import Historico
 
 def home(request):
     if request.method == 'POST':
@@ -10,3 +11,23 @@ def home(request):
             return render(request, 'base.html', {'erro': str(e), 'link': url})
         return render(request, 'base.html', {'result': result, 'link': url})
     return render(request, 'base.html')
+
+def home(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        try:
+            result = VirusTotalService().scan_url(url)
+
+            Historico.objects.create(
+                tipo='URL',
+                url=url,
+                resultado='Malicioso' if result['data']['attributes']['stats']['malicious'] > 0 else 'Limpo'
+            )
+        except Exception as e:
+            return render(request, 'base.html', {'erro': str(e), 'link': url})
+
+        historico = Historico.objects.all().order_by('-data')
+        return render(request, 'base.html', {'result': result, 'link': url, 'historico': historico})
+
+    historico = Historico.objects.all().order_by('-data')
+    return render(request, 'base.html', {'historico': historico})
