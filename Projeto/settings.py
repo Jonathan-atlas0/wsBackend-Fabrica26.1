65fd6ae0c00bc4a682d0dc674fbd34b669e39cc9
Jonathan <Jonathanbatista061@gmail.com>
2026-03-21 02:26:48 -0300

    feat(database): migra banco de dados para PostgreSQL

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'Projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'securedash',
+        'USER': 'postgres',
+        'PASSWORD': '2718',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
